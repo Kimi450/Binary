a3feff1f02eb1f645a31c313458d64406a4f5feb
Kimi450 <karan_samani@yahoo.co.in>
2017-01-05 23:22:13 +0000

    Minor changes 2
    Change log:
    1. File name has been change to BinaryVarPatterns.py from 1.0.py
    2. Update for better readability by adding more spaces
    3. 
    print(varLst)
    print("")
    has now been changed to
    print(varLst, "\n")
    for a cleaner code.

--- BinaryVarPatterns.py
+++ BinaryVarPatterns.py
@@ -1,14 +1,14 @@
 def Binary():
     #prints the list of the binary patterns for every variable to be used
     var = int(input("Please enter the number of variables: "))
-    print("")
+    print( "" )
     varNum = 1
     printCounter = int(var) #variable's position in the Truth Table
     for v in range(1,var+1):
-        finalLstLoop = 2 ** (var-varNum) - 1 #calculation for changing number of repetitions
+        finalLstLoop = 2 ** ( var - varNum ) - 1 #calculation for changing number of repetitions
         patternLst = []
         print("Variable repetition pattern's position in the Truth Table: ", printCounter)
-        form = int((2 ** varNum) / 2) #make sure they appear once or in multiples of 2
+        form = int( ( 2 ** varNum) / 2 ) #make sure they appear once or in multiples of 2
         for i in range(form): #prints of 1's for a variable
             patternLst.append(0)
         for i in range(form): #prints of 1's for a variable
@@ -17,8 +17,7 @@ def Binary():
         for patterns in range(finalLstLoop): #repeat the patterns to make them the same length
             for item in patternLst:
                 varLst.append(item)
-        print(varLst)
-        print("")
+        print(varLst, "\n\n")
         varNum += 1
         printCounter -= 1
         if varNum > var:
